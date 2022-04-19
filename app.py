f842ab27243a4b087190b6bb2e34bc8e49afe846
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-04-19 11:48:43 +0900

    renamed stack

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import aws_cdk as cdk
 from cdk_lambda_stack.lambda_api_stack import LambdaAPIStack
 
 app = cdk.App()
-LambdaAPIStack(app, "LambdaModelPredictionsStack")
+LambdaAPIStack(app, "ExampleLambdaAPIStack")
 
 app.synth()
